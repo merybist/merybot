6a4abdc0ae42927fd59cf7e5a2a72d3ac736f1e3
merybist <ostapchuk@DESKTOP-4JT95GP.tdk>
2025-04-24 18:34:29 +0300

    TTF

--- handlers/media/youtube_handler.py
+++ handlers/media/youtube_handler.py
@@ -206,7 +206,7 @@ def download_video_youtube(url, custom_label="youtube_video"):
             'format': 'bestvideo+bestaudio/best',
             'outtmpl': output_path,
             'merge_output_format': 'mp4',
-            'proxy': 'socks5:3.126.147.182:80',
+            'proxy': 'socks5://3.126.147.182:80',
             'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',  # Вкажіть ваш юзер-агент
             'quiet': True,
         }
