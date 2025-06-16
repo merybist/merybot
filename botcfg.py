2aea5293fccf9c7ee72ae31b90ba6d24ad4d6f20
merybist <86554244+Tribedn@users.noreply.github.com>
2025-06-16 04:29:27 +0300

    del:trash from utils.py | old telegram-api from botcfg.py

--- botcfg.py
+++ botcfg.py
@@ -2,8 +2,6 @@ import telebot
 import os
 
 bot = telebot.TeleBot('')
-telebot.apihelper.API_URL = 'http://localhost:3241/bot{0}/{1}'
 
-ADMIN_USER_ID = ""
 DOWNLOADS_FOLDER = os.path.abspath("downloads")
 
--- utils.py
+++ utils.py
@@ -1,28 +1,6 @@
 
 import os
 import re
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
-import base64
-import datetime
-from datetime import time
-
-def encrypt_aes(text, key):
-    cipher = AES.new(key, AES.MODE_CBC, key)
-    ciphertext = cipher.encrypt(pad(text.encode(), AES.block_size))
-    return base64.b64encode(ciphertext).decode()
-
-def decrypt_aes(encoded_text, key):
-    cipher = AES.new(key, AES.MODE_CBC, key)
-    decrypted = cipher.decrypt(encoded_text) 
-    return unpad(decrypted, AES.block_size).decode()
-
-def split_text(text, max_length=4096):
-    """
-    Розділяє текст на частини, кожна з яких не перевищує max_length символів.
-    """
-    return [text[i:i + max_length] for i in range(0, len(text), max_length)]
-
 
 def sanitize_filename(filename):
     """
